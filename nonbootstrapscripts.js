a54965f4bc718c57eb4eac584f4ada76c0dc5ce8
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-12-06 15:22:30 -0800

    Finished info layout, needs polishing but most things are there, Redid the drop down collapse and it works now

---
+++ nonbootstrapscripts.js
@@ -0,0 +1,7 @@
+// FOR READ MORE
+document.getElementById("sypnosis-btn").addEventListener("click", function (){
+    const sypnosis = document.getElementById("sypnosis")
+    sypnosis.classList.toggle("sypnosis-container")
+
+    
+} )
