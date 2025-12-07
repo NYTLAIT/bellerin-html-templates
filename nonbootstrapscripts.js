e70897f48f30699ec0b3851d55be89056c6f69f4
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-12-06 21:15:40 -0800

    Did Search Page, very basic layout, really needs polishing however, most everything complete now

--- nonbootstrapscripts.js
+++ nonbootstrapscripts.js
@@ -2,6 +2,27 @@
 document.getElementById("sypnosis-btn").addEventListener("click", function (){
     const sypnosis = document.getElementById("sypnosis")
     sypnosis.classList.toggle("sypnosis-container")
-
-    
 } )
+
+//FOR QUERY CONFIRM SOMEPOINT MAYBE
+/* document.getElementById("goal-type").addEventListener("select", function (){
+    const selectGoal = document.getElementById("select-goal-type")
+    const inputGoal = document.getAnimations("goal-input")
+    const startBtn = document.getAnimations("query-confirm-btn")
+    const selectDefaultOption = document.getElementById("select-goal-default")
+    
+    selectGoal.hidden = False
+    selectGoal.addEventListener("click", () => {
+        selectDefaultOption.hidden = True
+        if (selectGoal.value == "Time-Goal") {
+            inputGoal.innerHTML = "
+            <label>
+            "
+        }
+        else if (selectGoal.value == "Eps-Goal") {
+            inputGoal.innerHTML = "
+            
+            "
+        }
+    })
+}) */
